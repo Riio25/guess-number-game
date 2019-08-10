349d7a7b8ddb15d6cad52d6ca49e0fba1ca320e9
Riio <51425617+Riio25@users.noreply.github.com>
2019-08-10 16:57:35 -0600

    QA'd Game
    Added ifs to check and make sure entry was valid.
    Created restart button that restarts the game successfully.
    Added some colors.

--- js/index.js
+++ js/index.js
@@ -6,26 +6,68 @@ function game(){
 
 var guess = document.getElementById("guess").value;
 
-if ( lives > 1){
-  if (theNumber == guess){
-    document.getElementById("win").style.display = "block";
-  } else {
-  	lives--
-    document.getElementById("tryAgain").style.visibility = "visible";
-    document.getElementById("lives").innerHTML = lives;
+
+
+if (guess === ""){
+  alert("Please enter a number between 1-10")
+} else {
+  if (isNaN(guess)){
+    alert('Please enter a number between 1-10.')
+  } else{
+    if (guess > 10 || guess < 0){
+      alert('Please enter a number between 1-10')
+    } else {
+      if ( lives > 1){
+        if (theNumber == guess){
+          document.getElementById("gameover").innerHTML = "You Win!";
+          document.getElementById("gameover").style.display = "block";
+          document.getElementById("gameover").style.color = "green";
+          document.getElementById("answer").disabled = "true";
+          document.getElementById("restart").style.visibility = "visible";
+          document.getElementById("guess").style.color = "green";
+        } else {
+          lives--
+          document.getElementById("tryAgain").style.visibility = "visible";
+          document.getElementById("lives").innerHTML = lives;
+        }
+        } else {
+          document.getElementById("gameover").innerHTML = "You Lose!";
+          document.getElementById("gameover").style.display = "block";
+          document.getElementById("gameover").style.color = "red";
+          document.getElementById("answer").disabled = "true";
+          document.getElementById("restart").style.visibility = "visible";
+          document.getElementById("guess").style.color = "red";
+          document.getElementById("lives").innerHTML = "0";
+        }
+      }
+    }
   }
-  } else {
-  document.getElementById("lose").style.display = "block";
-  }
+}
+
+function restart(){
+  document.getElementById("gameover").style.display = "none";
+  document.getElementById("tryAgain").style.visibility = "hidden";
+  document.getElementById("restart").style.visibility = "hidden";
+  document.getElementById("lives").innerHTML = "3";
+  document.getElementById("answer").disabled = false;
+  document.getElementById("guess").value = "Make your guess..."
+  document.getElementById("guess").style.color = "white";
+  lives = 3;
+
 
 }
 
-document.getElementById("win").style.display = "none";
-document.getElementById("lose").style.display = "none";
+document.getElementById("gameover").style.display = "none";
 document.getElementById("tryAgain").style.visibility = "hidden";
+document.getElementById("restart").style.visibility = "hidden";
+
 
 document.getElementById("answer").onclick = function () {
 	game();
+}
+
+document.getElementById("restart").onclick = function() {
+  restart();
 }
 
 
